9745f60d62464212fafe83c2647f46af539e3545
rishi1601 <rishiwork1@gmail.com>
2017-04-16 12:53:53 +0530

    Db creation , User details added #1

--- Requirement/HotelBooking_initial_19_03_2017.sql
+++ Requirement/HotelBooking_initial_19_03_2017.sql
@@ -170,4 +170,111 @@ ALTER TABLE admin_social_networks ADD CONSTRAINT fk_social_hotel_id FOREIGN KEY 
 
 
 
+//16-04-2017
+
+
+CREATE TABLE IF NOT EXISTS user_info(
+user_id BIGINT(11) NOT NULL AUTO_INCREMENT,
+user_unique_id VARCHAR(255) UNIQUE NOT NULL,
+user_name VARCHAR(255) NOT NULL,
+user_email VARCHAR(255) UNIQUE NOT NULL,
+user_address_1 VARCHAR(255) NOT NULL,
+user_address_2 VARCHAR(255) NOT NULL,
+user_contact_no_1 VARCHAR(100) NOT NULL,
+user_contact_no_2 VARCHAR(100) NOT NULL,
+user_city VARCHAR(100) NOT NULL,
+user_state VARCHAR(100) NOT NULL,
+user_country VARCHAR(100) NOT NULL,
+user_secondary_email VARCHAR(255) NOT NULL,
+is_verified INT(3) NOT NULL DEFAULT 0 COMMENT '0=> Not Verified , 1=>Verified , 2 => Pending Verification',
+is_active INT(3) NOT NULL DEFAULT 0 COMMENT '0=> Active , 1=>In-Active',
+is_blocked INT(3) NOT NULL DEFAULT 0 COMMENT '0=> Not Blocked , 1=>Blocked',
+db_add_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+db_update_date DATETIME NOT NULL, 
+PRIMARY KEY (user_id)
+)
+
+
+
+
+CREATE TABLE IF NOT EXISTS user_social_network(
+user_social_network_id BIGINT(11) NOT NULL AUTO_INCREMENT,
+user_id BIGINT(11) NOT NULL,
+social_network_name VARCHAR(100) NOT NULL,
+social_network_identity VARCHAR(100) NOT NULL,
+db_add_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+db_update_date DATETIME NOT NULL, 
+PRIMARY KEY (user_social_network_id)
+)
+
+
+
+
+CREATE TABLE IF NOT EXISTS user_review(
+user_reviw_id BIGINT(11) NOT NULL AUTO_INCREMENT,
+user_id BIGINT(11) NOT NULL,
+hotel_id BIGINT(11) NOT NULL,
+room_id BIGINT(11) NOT NULL,
+booking_id BIGINT(11) NOT NULL,
+rating INT(11) NOT NULL,
+user_comment_title VARCHAR(255) ,
+user_comment TEXT,
+admin_reply_title VARCHAR(255),
+admin_reply TEXT,
+user_comment_add_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+user_comment_update_date DATETIME NOT NULL,
+admin_reply_add_date DATETIME NOT NULL,
+admin_reply_update_date DATETIME NOT NULL,
+db_add_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+db_update_date DATETIME NOT NULL, 
+PRIMARY KEY (user_reviw_id)
+)
+
+
+CREATE TABLE IF NOT EXISTS user_booking(
+booking_id BIGINT(11) NOT NULL AUTO_INCREMENT,
+booking_unique_id VARCHAR(255) NOT NULL,
+user_id BIGINT(11) NOT NULL,
+hotel_id BIGINT(11) NOT NULL,
+room_id BIGINT(11) NOT NULL,
+booking_date DATETIME NOT NULL,
+booking_custom_message TEXT,
+checkout_date DATETIME NOT NULL,
+checkout_custom_message TEXT , 
+booking_room_rate VARCHAR(100) NOT NULL,
+booking_room_quantity VARCHAR(100) NOT NULL,
+booking_room_discount VARCHAR(100) NOT NULL,
+booking_final_price VARCHAR(100) NOT NULL,
+booking_is_paid VARCHAR(100) NOT NULL,
+db_add_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+db_update_date DATETIME NOT NULL, 
+PRIMARY KEY (booking_id)
+)
+
+
+ALTER TABLE user_social_network ADD CONSTRAINT fk_user_social_user_id FOREIGN KEY (user_id) REFERENCES user_info(user_id);
+
+
+ALTER TABLE user_review ADD CONSTRAINT fk_user_review_user_id FOREIGN KEY (user_id) REFERENCES user_info(user_id);
+ALTER TABLE user_review ADD CONSTRAINT fk_user_review_hotel_id FOREIGN KEY (hotel_id) REFERENCES hotel_info(hotel_id);
+ALTER TABLE user_review ADD CONSTRAINT fk_user_social_room_id FOREIGN KEY (room_id) REFERENCES room_info(room_id);
+ALTER TABLE user_review ADD CONSTRAINT fk_user_social_booking_id FOREIGN KEY (booking_id) REFERENCES user_booking(booking_id);
+
+
+ALTER TABLE user_booking ADD CONSTRAINT fk_user_booking_user_id FOREIGN KEY (user_id) REFERENCES user_info(user_id);
+ALTER TABLE user_booking ADD CONSTRAINT fk_user_booking_hotel_id FOREIGN KEY (hotel_id) REFERENCES hotel_info(hotel_id);
+ALTER TABLE user_booking ADD CONSTRAINT fk_user_booking_room_id FOREIGN KEY (room_id) REFERENCES room_info(room_id);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
